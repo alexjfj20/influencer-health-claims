abf19bd2640cc2b370ab195b9faca7a027c4c850
alexjfj20 <auditsemseo@gmail.com>
2025-01-15 19:15:46 -0500

    Descripción añadir lib

--- src/frontend/src/components/InfluencerLeaderboard.vue
+++ src/frontend/src/components/InfluencerLeaderboard.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref, onMounted } from 'vue'
-import { supabase } from '../../../services/supabaseClient'
+import { supabase } from '../lib/supabaseClient'
 
 const influencers = ref([])
 const loading = ref(true)
